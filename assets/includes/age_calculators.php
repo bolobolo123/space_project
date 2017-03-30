8386a4ea95b4473bc9484636fc5a8f26920fa276
AmrheinA <amrheinantoine@gmail.com>
2017-03-30 14:29:38 +0200

    Adding formated API files

--- assets/includes/age_calculators.php
+++ assets/includes/age_calculators.php
@@ -3,16 +3,15 @@
  * ============= Dates processing and formating ==============
  */
 
-    $date_of_birth    = date_create($_POST["date_of_birth"]);
-    $date_incremented = strtotime("+1 day", strtotime($_POST["date_of_birth"]));
-    $date_incremented = date("Y-m-d", $date_incremented);
+    $date_of_birth    = date_create($_POST['date_of_birth']);
+    $date_incremented = strtotime('+1 day', strtotime($_POST['date_of_birth']));
+    $date_incremented = date('Y-m-d', $date_incremented);
     $timestamp        = time();
     $current_year     = date('Y');
     $year_of_birth    = date_format($date_of_birth, 'Y');
     $month_of_birth   = date_format($date_of_birth, 'm');
     $day_of_birth     = date_format($date_of_birth, 'd');
     $age              = $current_year - $year_of_birth;
-
 
 /**
  * ===================== Dates formating =====================
@@ -23,16 +22,30 @@
     $date_formated_UFO_2     = date_format(date_create($date_incremented), 'Y/m/d');
     $date_formated_trivia    = date_format($date_of_birth, 'Ymd');
     $date_ts_formated_trivia = date('Ymd');
+    $date_formated_launch    = date('Y-m-d');
 
 /**
  * =================== Dates manipulation =====================
  */
 
-    $compare_bigbang   = number_format((($age * 100)/1.35E10), 8);
-    $compare_milky     = number_format((($age * 100)/1.32E10), 8);
-    $compare_earth     = number_format((($age * 100)/4.57E9), 8);
-    $compare_rotations = number_format((($age * 365)/0.99726968), 2);
+    $data_compare_bigbang   = number_format((($age * 100)/1.35E10), 8);
+    $data_compare_milky     = number_format((($age * 100)/1.32E10), 8);
+    $data_compare_earth     = number_format((($age * 100)/4.57E9), 8);
+    $data_compare_rotations = number_format((($age * 365)/0.99726968), 2);
 
 /**
  * ============================================================
  */
+
+echo '<pre>';
+//print_r($result);
+echo '<h4>/////////////////////////////////////Age calculators API/////////////////////////////////////</h4>';
+echo '<br/>';
+print_r('Your existence represents '.$data_compare_bigbang.'% of the bigbang age');
+echo '<br/>';
+print_r('Your existence represents '.$data_compare_milky.'% of the milky age');
+echo '<br/>';
+print_r('Your existence represents '.$data_compare_earth.'% of the earth age' );
+echo '<br/>';
+print_r('During your existence the earth rotated '.$data_compare_rotations.' times');
+echo '</pre>';
--- index.php
+++ index.php
@@ -3,28 +3,61 @@
 	include 'assets/includes/age_calculators.php';
 //	include 'assets/includes/astrology_test.php'; // Disabled until a permanent API key is found.
 //	include 'assets/includes/geolocalisation.php'; // Not used while Astrology is disabled
-	include 'assets/includes/exoplanets.php';
-// 	include 'assets/includes/launch_count.php'; // Disabled until a solution is found (see file's comments)
+//	include 'assets/includes/exoplanets.php';
+ 	include 'assets/includes/launch_count.php';
 	include 'assets/includes/ovni_sightings.php';
 	include 'assets/includes/trivia.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Mon Site</title>
-	<!-- META -->
+    <title>Home</title>
+    <!-- META -->
     <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
     <meta description="">
-	<!-- CSS -->
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900" rel="stylesheet">
+    <!-- CSS -->
     <link rel="stylesheet" href="assets/styles/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Space+Mono:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
 </head>
 <body>
-<h3>Please enter your date of birth :</h3>
-    <form method="post">
-        <input type="date" name="date_of_birth">
-        <input type="submit">
-    </form>
+<div class="container-door">
+    <div class="left">
+        <div class="logo">
+            <img src="assets/img/logo.png" alt="spaceviz-logo">
+        </div>
+    </div>
+    <div class="right"></div>
+    <div class="container-birth">
+        <form action="#" method="GET" class="form-birth">
+            <div class="inputs">
+                <label for="name">ENTER YOUR NAME</label>
+                <input type="text" name="name" id="name" autofocus>
+                <label for="birth">ENTER YOUR BIRTHDATE</label>
+                <input type="date" name="birth" id="birth">
+            </div>
+            <div class="code-form">
+                <div class="numbers">
+                    <div class="number">1</div>
+                    <div class="number">2</div>
+                    <div class="number">3</div>
+                    <div class="number">4</div>
+                    <div class="number">5</div>
+                    <div class="number">6</div>
+                    <div class="number">7</div>
+                    <div class="number">8</div>
+                    <div class="number">9</div>
+                    <div class="number">0</div>
+                </div>
+                <div class="submit">
+                    <input type="submit" name="birthdate" value="VALID">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<!-- SCRIPTS -->
+<script src="assets/scripts/home.js"></script>
 </body>
 </html>
